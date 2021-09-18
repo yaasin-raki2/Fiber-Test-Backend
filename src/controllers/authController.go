66a376aac150e70e0a5b2205a8b1880a88c49674
yaasin-raki2 <yassin23raki@gmail.com>
2021-09-18 17:09:51 +0100

    setting up a basic mvc design with a basic post route

---
+++ src/controllers/authController.go
@@ -0,0 +1,9 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func Register(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"message": "hello",
+	})
+}
---
+++ src/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"ambassador/src/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+	api := app.Group("api")
+	admin := api.Group("admin")
+
+	admin.Post("/register", controllers.Register)
+}
