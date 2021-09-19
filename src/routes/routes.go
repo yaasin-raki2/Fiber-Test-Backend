f6a0e26c09fe4cc800dfbee6ee3fd79f13f00a3b
yaasin-raki2 <yassin23raki@gmail.com>
2021-09-19 18:42:15 +0100

    Extracting the logic behind scanning a cookie and it's jwt and extracting the user id from it to some middlewares

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -2,16 +2,19 @@ package routes
 
 import (
 	"ambassador/src/controllers"
+	"ambassador/src/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Setup(app *fiber.App) {
 	api := app.Group("api")
-	admin := api.Group("admin")
 
+	admin := api.Group("admin")
 	admin.Post("/register", controllers.Register)
 	admin.Post("/login", controllers.Login)
-	admin.Get("/user", controllers.User)
-	admin.Post("/logout", controllers.Logout)
+
+	adminAuthenticated := admin.Use(middlewares.IsAuthenticated)
+	adminAuthenticated.Get("/user", controllers.User)
+	adminAuthenticated.Post("/logout", controllers.Logout)
 }
